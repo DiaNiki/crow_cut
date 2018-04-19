5ce43c1a6c4ff5d783b6326c1b819fb2963cd09f
Diana Nykytiuk <nykytiuk.diana@gmail.com>
2018-04-19 12:28:58 +0300

    Fixes some mistyped variables in the algorithm, modifies a couple of parameters for better performance.

--- app/src/main/java/com/diana/crowcut/GrowCut.java
+++ app/src/main/java/com/diana/crowcut/GrowCut.java
@@ -9,8 +9,9 @@ import android.renderscript.RenderScript;
 class GrowCut {
     public static final int FOREGROUND_COLOUR = Color.RED;
     public static final int BACKGROUND_COLOUR = Color.BLUE;
-    private static final int[] nighX = { 1, -1, 0, 0 };
-    private static final int[] nighY = { 0, 0, 1, -1 };
+    private static final int WINDOW_SIZE = 3;
+    private static final int MAX_ITERATIONS = 30;
+    private static final long MAX_EXEC_TIME_MS = 2 * 60 * 1000;
 
     static Bitmap run(Context context, Bitmap imageData, Bitmap guides, Boolean isParallel)
     {
@@ -30,10 +31,8 @@ class GrowCut {
         int[][][] labels = new int[w][h][2];
         double[][][] strengths = new double[w][h][2];
         int step = 0;
-        double maxCellNorm2 = 0;
         for (int cellX = 0; cellX < w; ++cellX) {
             for (int cellY = 0; cellY < h; ++cellY) {
-                maxCellNorm2 = Math.max(maxCellNorm2, getNorm2(imageData.getPixel(cellX, cellY)));
                 switch (guides.getPixel(cellX, cellY)) {
                     case FOREGROUND_COLOUR:
                         labels[cellX][cellY][step] = 2;
@@ -51,30 +50,32 @@ class GrowCut {
             }
         }
 
-        for (int countSteps = 0; countSteps < 5; ++countSteps) {
+        final long startTime = System.currentTimeMillis();
+        final int ws = (WINDOW_SIZE + 1) >> 1;
+        for (int countSteps = 0; countSteps < MAX_ITERATIONS; ++countSteps) {
             step = 1 - step;
             Boolean wasModified = false;
             for (int cellX = 0; cellX < w; ++cellX) {
                 for (int cellY = 0; cellY < h; ++cellY) {
                     labels[cellX][cellY][step] = labels[cellX][cellY][1 - step];
                     strengths[cellX][cellY][step] = strengths[cellX][cellY][1 - step];
-                    for (int k = 0; k < nighX.length; ++k) {
-                        int neighX = cellX + nighX[k];
-                        int neighY = cellY + nighY[k];
-                        if (neighX >= 0 && neighY >= 0 && neighX < w && neighY < h) {
+                    neighbourLoop:
+                    for (int neighX = Math.max(0, cellX - ws); neighX < Math.min(cellX + ws + 1, w); ++neighX) {
+                        for (int neighY = Math.max(0, cellY - ws); neighY < Math.min(cellY + ws + 1, h); ++neighY) {
                             double attackingStrength = strengths[neighX][neighY][1 - step];
                             attackingStrength *= 1 - getNorm2(imageData.getPixel(cellX, cellY),
-                                    imageData.getPixel(neighX, neighY)) / maxCellNorm2;
-                            if (attackingStrength > strengths[cellX][cellY][1 - step] + 1e-2) {
-                                labels[cellX][cellY][step] = labels[neighX][neighX][1 - step];
+                                    imageData.getPixel(neighX, neighY)) / Math.sqrt(3.0);
+                            if (attackingStrength > strengths[cellX][cellY][step] + 1e-2) {
+                                labels[cellX][cellY][step] = labels[neighX][neighY][1 - step];
                                 strengths[cellX][cellY][step] = attackingStrength;
                                 wasModified = true;
+                                break neighbourLoop;
                             }
                         }
                     }
                 }
             }
-            if (!wasModified) {
+            if (!wasModified || System.currentTimeMillis() - startTime > MAX_EXEC_TIME_MS) {
                 break;
             }
         }
@@ -90,10 +91,6 @@ class GrowCut {
         return result;
     }
 
-    protected static double getNorm2(int colour) {
-        return getNorm2(Color.red(colour), Color.green(colour), Color.blue(colour));
-    }
-
     protected static double getNorm2(int colour1, int colour2) {
         return getNorm2(
                 Color.red(colour1) - Color.red(colour2),
@@ -102,6 +99,7 @@ class GrowCut {
     }
 
     protected static double getNorm2(double r, double g, double b) {
+        r /= 255; g /= 255; b /= 255;
         return Math.sqrt(r * r + g * g + b * b);
     }
 
